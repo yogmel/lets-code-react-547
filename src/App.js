ae31e8023b25756916f3dfa0ac50d48c425c971e
Mellina Y <yonashiro.mellina@gmail.com>
2021-04-21 23:13:32 -0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -23,29 +23,39 @@ import CitacaoContainer from "./routes/exercicioUm";
 // componentes funcionais ou stateLESS components
 function App() {
   return (
-  <div>
-  <BrowserRouter>
-    <p>Nossa galera de exercicios</p>
-    <Link to='/home'>Home</Link> | 
-    <Link to='/exercicioUm'>Exercicio Um</Link> | 
-    <Link to='/exercicioDois/Ricardo/12345'>Exercicio Dois</Link> |
-    <Link to='/exercicioTres'>Exercicio Três</Link> |
-    <Link to='/exercicioQuatro'>Exercicio Quatro</Link> |
-    <Link to='/exercicioCinco'>Exercicio Cinco</Link> |
-    <Link to='/exercicioSeis'>Exercicio Seis</Link> |
-    <Link to='/exercicioSete'>Exercicio Sete</Link> |
-    <Switch>
-      <Route path='/exercicioUm'><CitacaoContainer/></Route>
-      <Route path='/exercicioDois/:nome/:cpf'><GeradorDocumento/></Route>
-      <Route path='/exercicioTres'><NumAleatorio/></Route>
-      <Route path='/exercicioQuatro'><PrevisaoDoTempoContainer/></Route>
-      <Route path='/exercicioCinco'><ChatContainer/></Route>
-      <Route path='/exercicioSeis'><ListaTarefas/></Route>
-      <Route path='/exercicioSete'><Giphy/></Route>
-      <Route exact={true} path='/'>Home dos exercicios!!</Route>
-    </Switch>
-  </BrowserRouter>
-  </div>);
+    <div>
+      <BrowserRouter>
+        <p>Nossa galera de exercicios</p>
+        <Navbar />
+        <Switch>
+          <Route path="/exercicioUm">
+            <CitacaoContainer />
+          </Route>
+          <Route path="/exercicioDois/:nome/:cpf">
+            <GeradorDocumento />
+          </Route>
+          <Route path="/exercicioTres">
+            <NumAleatorio />
+          </Route>
+          <Route path="/exercicioQuatro">
+            <PrevisaoDoTempoContainer />
+          </Route>
+          <Route path="/exercicioCinco">
+            <ChatContainer />
+          </Route>
+          <Route path="/exercicioSeis">
+            <ListaTarefas />
+          </Route>
+          <Route path="/exercicioSete">
+            <Giphy />
+          </Route>
+          <Route exact={true} path="/">
+            Home dos exercicios!!
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <Link to="/home">Home</Link> |<Link to="/exercicioUm">Exercicio Um</Link>{" "}
+      |<Link to="/exercicioDois/Ricardo/12345">Exercicio Dois</Link> |
+      <Link to="/exercicioTres">Exercicio Três</Link> |
+      <Link to="/exercicioQuatro">Exercicio Quatro</Link> |
+      <Link to="/exercicioCinco">Exercicio Cinco</Link> |
+      <Link to="/exercicioSeis">Exercicio Seis</Link> |
+      <Link to="/exercicioSete">Exercicio Sete</Link> |
+    </>
+  );
+};
+
+export default Navbar;
